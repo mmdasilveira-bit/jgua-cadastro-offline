10487b94c5ade780bbcbc840cdc3bc71f73cc23f
mmdasilveira-bit <mmdasilveira@gmail.com>
2025-12-24 14:05:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-indexedDB.deleteDatabase("JGUA_DB");
+//indexedDB.deleteDatabase("JGUA_DB");
 //https://script.google.com/macros/s/AKfycbziH71TxS7YCz_-b8SjbjtXi1dLO0TTYmAHJF5vBHUmMrmo-ujJxHif0aY3ZOQduv552Q/exec
 const URL_PLANILHA = "https://script.google.com/macros/s/AKfycbziH71TxS7YCz_-b8SjbjtXi1dLO0TTYmAHJF5vBHUmMrmo-ujJxHif0aY3ZOQduv552Q/exec"; 
 
